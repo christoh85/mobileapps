14c03a335bdf1bbd4cda545a10b51ccf7afa88d8
root <root@localhost.localdomain>
2016-03-08 20:12:30 +0200

    Initial commit

---
+++ blog/blogpost84.sh
@@ -0,0 +1,288 @@
+Arts & Entertainment
+Autos & Vehicles
+Beauty & Fitness
+Books & Literature
+Business & Industrial
+Computers & Electronics
+Finance
+Food & Drink
+Games
+Hobbies & Leisure
+Home & Garden
+Internet & Telecom
+Jobs & Education
+Law & Government
+News
+Online Communities
+People & Society
+Pets & Animals
+Real Estate
+Reference
+Science
+Shopping
+Sports
+Travel
+World Localities
+Celebrities & Entertainment News
+Comics & Animation
+Anime & Manga
+Cartoons
+Comics
+Entertainment Industry
+Film & TV Industry
+Film & TV Awards
+Film & TV Production
+Recording Industry
+Music Awards
+Record Labels
+Events & Listings
+Bars, Clubs & Nightlife
+Concerts & Music Festivals
+Event Ticket Sales
+Expos & Conventions
+Film Festivals
+Food & Beverage Events
+Live Sporting Events
+Movie Listings & Theater Showtimes
+Fun & Trivia
+Flash-Based Entertainment
+Fun Tests & Silly Surveys
+Humor
+Live Comedy
+Political Humor
+Spoofs & Satire
+Movies
+Action & Adventure Films
+Martial Arts Films
+Superhero Films
+Western Films
+Animated Films
+Bollywood & South Asian Film
+Classic Films
+Silent Films
+Comedy Films
+Cult & Indie Films
+DVD & Video Shopping
+DVD & Video Rentals
+Documentary Films
+Drama Films
+Family Films
+Horror Films
+Movie Memorabilia
+Movie Reference
+Movie Reviews & Previews
+Musical Films
+Romance Films
+Science Fiction & Fantasy Films
+Thriller, Crime & Mystery Films
+Music & Audio
+CD & Audio Shopping
+Classical Music
+Country Music
+Dance & Electronic Music
+Experimental & Industrial Music
+Folk & Traditional Music
+Jazz & Blues
+Blues
+Jazz
+Music Art & Memorabilia
+Music Education & Instruction
+Music Equipment & Technology
+DJ Resources & Equipment
+Music Recording Technology
+Musical Instruments
+Drums & Percussion
+Guitars
+Pianos & Keyboards
+Samples & Sound Libraries
+Music Reference
+Music Composition & Theory
+Sheet Music
+Song Lyrics & Tabs
+Music Streams & Downloads
+Music Videos
+Pop Music
+Radio
+Podcasting
+Talk Radio
+Rock Music
+Classic Rock & Oldies
+Hard Rock & Progressive
+Indie & Alternative Music
+Metal (Music)
+Punk (Music)
+Soundtracks
+Urban & Hip-Hop
+Rap & Hip-Hop
+Reggaeton
+Soul & R&B
+Vocals & Show Tunes
+World Music
+African Music
+Arab & Middle Eastern Music
+East Asian Music
+Latin American Music
+Brazilian Music
+Latin Pop
+Salsa & Tropical Music
+Reggae & Caribbean Music
+South Asian Music
+Offbeat
+Online Media
+Online Image Galleries
+Webcams & Virtual Tours
+Performing Arts
+Acting & Theater
+Broadway & Musical Theater
+Circus
+Dance
+Magic
+Opera
+TV & Video
+Online Video
+Web Series
+TV Commercials
+TV Guides & Reference
+TV Networks & Stations
+TV Shows & Programs
+TV Comedies
+TV Documentary & Nonfiction
+TV Dramas
+TV Crime & Legal Shows
+TV Medical Shows
+TV Soap Operas
+TV Family-Oriented Shows
+TV Game Shows
+TV Reality Shows
+TV Sci-Fi & Fantasy Shows
+TV Talent & Variety Shows
+TV Talk Shows
+Visual Art & Design
+Architecture
+Art & Craft Supplies
+Art Museums & Galleries
+Arts Education
+Design
+Graphic Design
+Industrial & Product Design
+Interior Design
+Painting
+Photographic & Digital Arts
+Sculpture
+Advertising & Marketing
+Brand Management
+Marketing
+Promotional Items & Corporate Gifts
+Public Relations
+Sales
+Telemarketing
+Aerospace & Defense
+Defense Industry
+Space Technology
+Agriculture & Forestry
+Agricultural Equipment
+Aquaculture
+Crops & Seed
+Farms & Ranches
+Food Production
+Forestry
+Horticulture
+Livestock
+Automotive Industry
+Business Education
+Business Finance
+Commercial Lending
+Investment Banking
+Risk Management
+Venture Capital
+Business Operations
+Business Plans & Presentations
+Human Resources
+Compensation & Benefits
+Corporate Training
+Payroll Services
+Recruitment & Staffing
+Management
+Business Process
+Project Management
+Strategic Planning
+Supply Chain Management
+Business Services
+Consulting
+Corporate Events
+Trade Fairs & Industry Shows
+E-Commerce Services
+Merchant Services & Payment Systems
+Fire & Security Services
+Knowledge Management
+Office Services
+Office & Facilities Management
+Office Supplies
+Office Furniture
+Outsourcing
+Physical Asset Management
+Quality Control & Tracking
+Signage
+Writing & Editing Services
+Chemicals Industry
+Agrochemicals
+Cleaning Agents
+Coatings & Adhesives
+Dyes & Pigments
+Plastics & Polymers
+Construction & Maintenance
+Nails Screws & Fasteners
+Plumbing Fixtures & Equipment
+Wood & Plastics
+Civil Engineering
+Construction Consulting & Contracting
+Urban & Regional Planning
+Energy & Utilities
+Electricity
+Nuclear Energy
+Oil & Gas
+Renewable & Alternative Energy
+Hydropower
+Solar Power
+Wind Power
+Waste Management
+Waste Recycling
+Water Supply & Treatment
+Event Planning
+Food Service
+Restaurant Supply
+Industrial Materials & Equipment
+Valves Hoses & Fittings
+Generators
+Heavy Machinery
+Manufacturing
+Factory Automation
+Metals & Mining
+Precious Metals
+Pharmaceuticals & Biotech
+Printing & Publishing
+Document & Printing Services
+Business Cards & Stationary
+Professional & Trade Associations
+Retail Trade
+Retail Equipment & Technology
+Small Business
+Business Formation
+Home Office
+MLM & Business Opportunities
+Textiles & Nonwovens
+Transportation & Logistics
+Aviation
+Distribution & Logistics
+Freight & Trucking
+Import & Export
+Mail & Package Delivery
+Couriers & Messengers
+Maritime Transport
+Moving & Relocation
+Packaging
+Parking
+Public Storage
+Rail Transport
+Urban Transport
+Warehousing
